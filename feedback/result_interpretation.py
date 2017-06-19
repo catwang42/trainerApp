46773f82173ee3aced60723f69e4061a26577bb4
Zhizhen Wang <catherine_wang@Zhizhens-MacBook-Pro.local>
2017-06-19 21:12:15 +1000

    init

---
+++ feedback/result_interpretation.py
@@ -0,0 +1,78 @@
+from collections import Counter
+import sys
+sys.path.append('feedback')
+import advice_messages
+
+def advice(exercise, results):
+	messages = advice_messages.messages[exercise]
+	categories = advice_messages.messages[exercise+'_categories']
+	
+	major_problems = []
+	minor_problems = []
+	strengths = []
+
+	#=====[ Iterate through each component  ]=====
+	for key in results:
+		num_reps = len(results[key])
+		reps = (Counter(results[key])).most_common()
+
+		#=====[ Iterate through each rep  ]=====
+		for counts in reps:
+			
+			severity = float(counts[1])/num_reps
+			
+			#=====[ Decide if label for rep indicates problem area  ]=====
+			if counts[0] != 0:
+
+				#=====[ Decide if label for rep indicates major problem area  ]=====
+				if severity > 0.5:
+					major_problems.append(key)
+
+				elif severity > 0.25:
+					minor_problems.append(key)
+
+			else:
+				
+				#=====[ Decide if label indicates strong performance in category  ]=====
+				if severity > 0.9:
+					strengths.append(key)
+
+	# print_problems(major_problems, messages, categories, 'Major')
+	# print_problems(minor_problems, messages, categories, 'Minor')
+	# print_strengths(strengths, messages, categories)
+	advice = []
+	advice.append('A total of ' + str(num_reps) + ' reps were analyzed')
+	advice.append(get_problems(major_problems, messages, categories, 'Major'))
+	advice.append(get_problems(minor_problems, messages, categories, 'Minor'))
+	advice.append(get_strengths(strengths, messages, categories))
+	return advice 
+	
+def get_problems(problems, messages, categories, problem_type):
+	output = ''				
+	if len(problems) > 0:
+		output += '\n\n' + problem_type + ' Problems:\n\n'
+		for problem in problems:
+			output += categories[problem] + ":" + messages[problem] + '\n'
+	return output
+
+def get_strengths(strengths, messages, categories):
+	output = ''
+	if len(strengths) > 0:
+		output += '\n\nStrengths:\n\n'
+		for strength in strengths:
+			output+= categories[strength] + '\n'
+	return output
+
+def print_problems(problems, messages, categories, problem_type):				
+	if len(problems) > 0:
+		print '\n\n' + problem_type + ' Problems:\n'
+		for problem in problems:
+			print categories[problem], ":", messages[problem]
+
+def print_strengths(strengths, messages, categories):
+	if len(strengths) > 0:
+		print '\n\nStrengths:\n'
+		for strength in strengths:
+			print categories[strength]
+
+
---
+++ server.py
@@ -0,0 +1,84 @@
+import os
+import sys
+import pickle
+
+#=====[ Import inference tools  ]=====
+sys.path.append('inference')
+from ai_trainer import Personal_Trainer
+import classification_ftopt
+import utils as ut
+
+#=====[ Flask ]=====
+from flask import Flask
+from flask import request
+from flask import render_template
+from flask import send_from_directory
+from flask import abort
+from flask import make_response
+from flask import redirect
+from flask import render_template
+from flask import url_for
+from flask import flash
+
+#=====[ WebApp Setup ]=====
+base_dir = os.path.split(os.path.realpath(__file__))[0]
+static_dir = os.path.join(base_dir, 'static')
+app = Flask(__name__, static_folder=static_dir)
+results = []
+
+#=====[ Squat Inference Setup ]=====
+pt = Personal_Trainer({'squat':'NeckY','pushup':'NeckY'}, auto_start=True)
+
+################################################################################
+####################[ HANDLING REQUESTS ]#######################################
+################################################################################
+
+@app.route("/", methods=['GET'])
+def home():
+	return "Hi"
+
+@app.route("/analyze/<file_name>")
+def analyze(file_name):
+	
+	
+	#=====[ Analyze squat data to pull out normalized, spliced reps ]=====
+	squats = pt.analyze_reps('squat', file_name)
+	
+	#=====[ Extract feature vectors from squats for each exercise componenet  ]=====
+	squat_feature_vectors = pt.get_prediction_features_opt('squat',squats)
+	
+	results = {}
+	#=====[ Run classification on each squat component and report results  ]=====
+	for key in squat_feature_vectors:
+	    X = squat_feature_vectors[key]
+	    classification = pt.classify('squat', key, X)
+	    results[key] = classification
+		
+	return results
+
+def advice():
+	results = analyze('squatData.txt')
+	output_advice = pt.get_advice('squat',results)
+	ut.print_success('Feedback retrieved')
+	advice_file = open('advice_file.txt','wb')
+	advice_file.write(output_advice)
+	advice_file.close()
+
+
+@app.route("/get_advice", methods=['GET'])
+def get_advice():
+	advice_file = open('advice_file.txt','wb')
+	return advice_file.read()
+
+@app.route("/analyze_raw", methods=['POST'])
+def analyze_raw():
+	to_write = open('squatData.txt','wb')
+	print request.data
+	# to_write.write(request.data)
+	# ut.print_success('Data written to file')
+	# advice()
+	# ut.print_success('Advice file populated')
+	return 'OK'
+
+if __name__ == '__main__':
+	app.run(host='0.0.0.0')
